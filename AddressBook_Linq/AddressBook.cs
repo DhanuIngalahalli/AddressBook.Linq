5b1c76c2462b4592238bf53b0844923d23e9df76
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-27 11:19:34 -0700

    Creating AddressBook Table

--- AddressBook_Linq/AddressBook.cs
+++ AddressBook_Linq/AddressBook.cs
@@ -11,6 +11,16 @@ namespace AddressBook_Linq
         {
             //DataTable 
             DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("AddressBookName", typeof(string));
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
+            dataTable.Columns.Add("AddressBookType", typeof(string));
         }
     }
 }
